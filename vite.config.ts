614ce03f01c7bfe7febd5ad8597be7e25b778330
shreedarshini <23UIT051@avinuty.ac.in>
2026-07-04 21:27:08 +0530

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base:'/shree-jewellerys/',
+    base:"/shree-jewellerys/",
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
